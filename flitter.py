59b4b488dfe418e29baa742bb36263ac4f35da74
Eason Chen <easoncxz@gmail.com>
2015-07-04 23:46:26 +1200

    Migrated from rauth to flask-oauthlib.

--- flitter.py
+++ flitter.py
@@ -7,46 +7,29 @@ from flask import Flask, render_template, redirect, url_for, abort
 from flask import request, session
 
 from utils import login_required
-from twitter_stuff import twitter
+from twitter_stuff import twitter, log_session_in
 
 app = Flask(__name__)
 
 @app.route('/')
 @login_required
 def index():
-    at, ats = session['user']['twitter_credentials']
-    oauth_sess = twitter.get_session(token=(at, ats))
-    resp = oauth_sess.get('account/verify_credentials.json')
-    screen_name = resp.json()['screen_name']
+    resp = twitter.get('account/verify_credentials.json')
+    screen_name = resp.data['screen_name']
     return render_template('home.html',
-            user=dict(
-                screen_name=screen_name,
-                at=at,
-                ats=ats))
+            user={'screen_name': screen_name})
 
 @app.route('/login/')
 def login():
-    rt, rts = twitter.get_request_token(
-            data={'oauth_callback': url_for(
-                    'oauth_callback',
-                    _external=True)})
-    session['twitter_temporary_credentials'] = rt, rts
-    return redirect(twitter.get_authorize_url(rt))
+    next = request.args.get('next', url_for('index'))
+    return twitter.authorize(
+            callback=url_for('oauth_callback', next=next))
 
 @app.route('/oauth_callback/')
 def oauth_callback():
-    rt = request.args.get('oauth_token')
-    veri = request.args.get('oauth_verifier')
-    if rt is None or veri is None:
-        abort(404)
-    rt_, rts = session['twitter_temporary_credentials']
-    assert rt == rt_
-    at, ats = twitter.get_access_token(rt, rts, method="POST",
-            data={'oauth_verifier': veri})
-    del session['twitter_temporary_credentials']
-    session['user'] = dict(twitter_credentials=(at, ats))
-    # Hacky: Should redirect back to original url!
-    return redirect(url_for('index'))
+    log_session_in()
+    next = request.args.get('next', url_for('index'))
+    return redirect(next)
 
 @app.route('/logout/')
 def logout():
@@ -55,6 +38,5 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.secret_key = os.urandom(24)
-    print("App secret key: {}".format(app.secret_key))
+    app.secret_key = os.getenv('FLASK_SECRET_KEY') or os.urandom(24)
     app.run(debug=True)
--- twitter_stuff.py
+++ twitter_stuff.py
@@ -1,13 +1,28 @@
 # coding: utf-8
 
 import os
-from rauth import OAuth1Service
+from flask import session
+from flask_oauthlib.client import OAuth
 
-twitter = OAuth1Service(
+_oauth = OAuth()
+_flask_oauthlib_twitter = _oauth.remote_app('twitter',
     consumer_key = os.getenv('TWITTER_CONSUMER_KEY'),
     consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET'),
-    name='twitter',
     access_token_url='https://api.twitter.com/oauth/access_token',
     authorize_url='https://api.twitter.com/oauth/authorize',
     request_token_url='https://api.twitter.com/oauth/request_token',
     base_url='https://api.twitter.com/1.1/')
+
+twitter = _flask_oauthlib_twitter
+
+# https://flask-oauthlib.readthedocs.org/en/latest/client.html
+@twitter.tokengetter
+def get_twitter_token():
+    return session.get('user', {}).get('twitter_credentials')
+
+# https://flask-oauthlib.readthedocs.org/en/latest/client.html
+def log_session_in():
+    """ Save OAuth AT/ATS info into session. """
+    resp = twitter.authorized_response()
+    at, ats = resp['oauth_token'], resp['oauth_token_secret']
+    session['user'] = {'twitter_credentials': (at, ats)}
